8b61444a3258a34c483a584f4981429b2ce9ac51
ewk8 <ewk8@live.com>
2017-09-24 22:08:59 +0800

    连接批评空间

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
+var request = require('request');
+const cheerio = require('cheerio')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '使用了Express！' });
+  request.post(
+    'http://www.erogamescape.org/~ap2/ero/toukei_kaiseki/sql_for_erogamer_form.php',
+    { form: { 'sql': "SELECT * FROM brandlist where id < 10 order by id"} } ,
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          const $ = cheerio.load(body);
+          var hhtml = $('#query_result_main').find('tr').get(0);
+          var dhtml = $('#query_result_main').find('tr').not(hhtml)
+          var data = {
+            header: [],
+            items: [],
+          }
+          $(hhtml).find('th').map(function(i, el) {
+            // this === el 
+            data.header.push($(el).text())
+          })
+
+          $(dhtml).map(function(i, el) {
+            var it = []
+            $(el).find('td').map(function(j, e) {
+              it.push($(e).text())
+            })
+            data.items.push(it)
+          })
+          res.send(JSON.stringify(data))
+        }
+    }
+  );
+  //res.render('index', { title: '使用了Express！' });
 });
 
 module.exports = router;
